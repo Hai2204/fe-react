5c49ca4c7bd971490eb7af2af996ab16c39e5f91
hainv2204 <hai.nguyen.grooo@mafc.com.vn>
2023-06-07 17:28:41 +0700

    commit: add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Page404 from 'page/error/page_404';
+import Home from 'page/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="123/" element={<Page404 />} />
+      </Routes>
+    </Router>
   );
 }
 
